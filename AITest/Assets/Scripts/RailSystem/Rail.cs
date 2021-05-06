aa926c978a3825d4c980df6eebab5aa21b3df383
Daniel Schurmann <2096633@student.swin.edu.au>
2021-05-07 03:45:58 +1000

    - started an attempt at a different rail system

---
+++ AITest/Assets/Scripts/RailSystem/Rail.cs
@@ -0,0 +1,40 @@
+using System;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class Rail : MonoBehaviour
+{
+    private Transform[] nodes;
+
+    void Start()
+    {
+        nodes = Array.FindAll(GetComponentsInChildren<Transform>(), NotThis);
+    }
+
+    public Vector3 Move(Vector3 pos, int seg, int dir, float speed)
+    {
+        Vector3 v1 = nodes[seg].position;
+        Vector3 v2 = nodes[seg + 1].position;
+
+        return Vector3.Lerp(v1, v2, speed);
+    }
+
+    //Onscreen display for editor
+    private void OnDrawGizmos()
+    {
+        for(int i = 0; i < nodes.Length; i++)
+        {
+            UnityEditor.Handles.DrawWireCube(nodes[i].position, Vector3.one);
+            if (i < nodes.Length - 1)
+            {
+                UnityEditor.Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 4f);
+            }
+        }
+    }
+
+    private bool NotThis(Transform t)
+    {
+        return t != this.transform;
+    }
+
+}
---
+++ AITest/Assets/Scripts/RailSystem/Train.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class Train : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private int dir;
+    private int segment = 0;
+    private float d;
+    [SerializeField] private Rail rail;
+
+    private void Update()
+    {
+        if(!rail)
+        {
+            return;
+        }
+
+        Move();
+    }
+
+    private void Move()
+    {
+        d += Time.deltaTime * 1 / 2.5f;
+        Debug.Log(segment);
+        if(d < 0)
+        {
+            dir = 1;
+            segment--;
+        }
+        else if(d > 1)
+        {
+            dir = 0 ;
+            segment++;
+        }
+
+        transform.position = rail.Move(transform.position, segment, dir, d);
+    }
+}
