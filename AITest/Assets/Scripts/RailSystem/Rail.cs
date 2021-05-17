5662760756bd3b9036f8882560360ceb33cb0c05
Daniel Schurmann <2096633@student.swin.edu.au>
2021-05-18 01:00:06 +1000

    - rail system is functional - uses legacy input system, needs to be converted - still a little jank, character doesn't go backwards between some nodes - character doesn't follow the path exact enough (this causes the above problem), needs some course correction

--- AITest/Assets/Scripts/RailSystem/Rail.cs
+++ AITest/Assets/Scripts/RailSystem/Rail.cs
@@ -6,9 +6,23 @@ public class Rail : MonoBehaviour
 {
     private Transform[] nodes;
 
-    void Start()
+    private void Awake()
     {
         nodes = Array.FindAll(GetComponentsInChildren<Transform>(), NotThis);
+    }
+
+    public Vector3 Rotate(int seg, float d)
+    {
+        Vector3 result = Vector3.zero;
+        if (d < 0)
+        {
+            result = nodes[seg].position;
+        }
+        else if (d > 0)
+        {
+            result = nodes[seg + 1].position;
+        }
+        return result;
     }
 
     public Vector3 Move(int seg, float speed)
@@ -17,6 +31,55 @@ public class Rail : MonoBehaviour
         Vector3 v2 = nodes[seg + 1].position;
 
         return Vector3.Lerp(v1, v2, speed);
+    }
+
+    public Vector3 CatmullMove(int seg, float speed)
+    {
+        Vector3 p1, p2, p3, p4;
+
+        if (seg == 0)
+        {
+            p1 = nodes[seg].position;
+            p2 = p1;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 1].position;
+        }
+        else if (seg == nodes.Length - 2)
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = p3;
+        }
+        else
+        {
+            p1 = nodes[seg - 1].position;
+            p2 = nodes[seg].position;
+            p3 = nodes[seg + 1].position;
+            p4 = nodes[seg + 2].position;
+        }
+
+        float t2 = speed * speed;
+        float t3 = t2 * speed;
+
+        float x = 0.5f * ((2.0f * p2.x) //this is catmull-rom equation see => https://www.mvps.org/directx/articles/catmull/ for details
+            + (-p1.x + p3.x) * speed 
+            + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2
+            + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
+
+        /*
+         float y = 0.5f * ((2.0f * p2.y)
+            + (-p1.y + p3.y) * speed 
+            + (2.0f * p1.y - 5.0f * p2.y + 4 * p3.y - p4.y)* t2
+            + (-p1.y + 3.0f * p2.y - 3.0f * p3.y + p4.y) * t3);
+        
+         */
+        float z = 0.5f * ((2.0f * p2.z)
+            + (-p1.z + p3.z) * speed 
+            + (2.0f * p1.z - 5.0f * p2.z + 4 * p3.z - p4.z) * t2 
+            + (-p1.z + 3.0f * p2.z - 3.0f * p3.z + p4.z) * t3);
+
+        return new Vector3(x, 0, z);
     }
 
     //Onscreen display for editor
@@ -30,11 +93,41 @@ public class Rail : MonoBehaviour
                 UnityEditor.Handles.DrawDottedLine(nodes[i].position, nodes[i + 1].position, 4f);
             }
         }
+
+        for(int i = 0; i < nodes.Length - 1; i++)
+        {
+            float temp = 0;
+            for(int j = 0; j < 20; j++)
+            {
+                float f = 0;
+                if(j > 0)
+                {
+                    f = (float)j / 20;
+                }
+                UnityEditor.Handles.DrawLine(CatmullMove(i, temp), CatmullMove(i, f + 0.05f));
+                temp = f;
+            }
+        }
     }
 
     private bool NotThis(Transform t)
     {
         return t != this.transform;
+    }
+
+    public Vector3 GetNodePos(int i)
+    {
+        return nodes[i].position;
+    }
+
+    public int NodeLength
+    {
+        get { return nodes.Length; }
+    }
+
+    public string getName(int i)
+    {
+        return nodes[i].name;
     }
 
 }
--- AITest/Assets/Scripts/RailSystem/Train.cs
+++ AITest/Assets/Scripts/RailSystem/Train.cs
@@ -1,11 +1,25 @@
 ﻿using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Train : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 2.5f;
-    private int segment = 0;
-    private float d;
+    [SerializeField] private float moveSpeed = 10f;
+    private float jumpHeight = 7.5f;
+    [SerializeField] private int segment = 0;
+    [SerializeField] private float d = 0;
     [SerializeField] private Rail rail;
+
+    [SerializeField] private float startDist;
+    private float y = 0;
+    private CharacterController cc;
+
+    public float percentage;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+        startDist = Vector3.Distance(transform.position, rail.GetNodePos(segment + 1));
+    }
 
     private void Update()
     {
@@ -19,18 +33,67 @@ public class Train : MonoBehaviour
 
     private void Move()
     {
-        d += Time.deltaTime * (Input.GetAxis("Horizontal") / moveSpeed);
-        if(d < 0)
+        //handle jumping
+        if (cc.isGrounded && Input.GetKeyDown(KeyCode.Space))
         {
-            dir = 1;
-            segment--;
+            y = jumpHeight;
+
         }
-        else if(d > 1)
+        else if (cc.isGrounded)
         {
-            dir = 0 ;
-            segment++;
+            y = Physics.gravity.y / 4f;
+        }
+        else
+        {
+            y += Physics.gravity.y * Time.deltaTime;
         }
 
-        transform.position = rail.Move(segment, d);
+        float input = Input.GetAxis("Horizontal");
+        Vector3 pos = new Vector3(transform.position.x, 0, transform.position.z);
+        //get catmull move
+        //get percentage of the distance of the player in the current segment
+        float dist = Vector3.Distance(rail.GetNodePos(segment), pos);
+        percentage = dist / startDist;
+
+        d = percentage + (input * 0.1f);
+        if (percentage > 1 && d > 1)
+        {
+            if (segment < rail.NodeLength - 2)
+            {
+                percentage = 0;
+                segment++;
+                startDist = Vector3.Distance(pos, rail.GetNodePos(segment + 1));
+            }
+        }
+        else if (percentage < 0.06 && d < 0)
+        {
+            if (segment > 0)
+            {
+                percentage = 1;
+                segment--;
+                startDist = Vector3.Distance(pos, rail.GetNodePos(segment));
+            }
+        }
+
+        //move
+        Vector3 catmullD = rail.CatmullMove(segment, d);
+        Vector3 catmullP = rail.CatmullMove(segment, percentage);
+        Vector3 offset = catmullD - pos;
+
+        offset = offset.normalized * moveSpeed;
+
+        Vector3 rot = rail.Rotate(segment, input);
+        Vector3 newRot = new Vector3(rot.x, transform.position.y, rot.z);
+        transform.LookAt(newRot);
+
+        Vector3 move = new Vector3(0, y, 0);
+
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            move = new Vector3(offset.x, y, offset.z);
+        }
+        //move = Vector3.forward * Input.GetAxis("Horizontal");
+
+        cc.Move(move * Time.deltaTime);
     }
 }
