254a6af1ed42e6fa8b4782779899884ae232b9fb
Matt Smith <102088891@student.swin.edu.au>
2021-06-06 21:10:33 +1000

    added menu, end game trigger and end scene

--- Sandbox/Assets/Scripts/Rails System/Rail.cs
+++ Sandbox/Assets/Scripts/Rails System/Rail.cs
@@ -1,11 +1,13 @@
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class Rail : MonoBehaviour
 {
+
     [Header("Node Properties")]
-    [Range(0.1f,1f)]
+    [Range(0.1f, 1f)]
     public float NodeSize = 0.25f;
 
     private Transform[] nodes;
@@ -67,7 +69,7 @@ public class Rail : MonoBehaviour
         float t3 = t2 * speed;
 
         float x = 0.5f * ((2.0f * p2.x) //this is catmull-rom equation see => https://www.mvps.org/directx/articles/catmull/ for details
-            + (-p1.x + p3.x) * speed 
+            + (-p1.x + p3.x) * speed
             + (2.0f * p1.x - 5.0f * p2.x + 4 * p3.x - p4.x) * t2
             + (-p1.x + 3.0f * p2.x - 3.0f * p3.x + p4.x) * t3);
 
@@ -79,8 +81,8 @@ public class Rail : MonoBehaviour
         
          */
         float z = 0.5f * ((2.0f * p2.z)
-            + (-p1.z + p3.z) * speed 
-            + (2.0f * p1.z - 5.0f * p2.z + 4 * p3.z - p4.z) * t2 
+            + (-p1.z + p3.z) * speed
+            + (2.0f * p1.z - 5.0f * p2.z + 4 * p3.z - p4.z) * t2
             + (-p1.z + 3.0f * p2.z - 3.0f * p3.z + p4.z) * t3);
 
         return new Vector3(x, 0, z);
@@ -89,7 +91,7 @@ public class Rail : MonoBehaviour
     //Onscreen display for editor
     private void OnDrawGizmos()
     {
-        for(int i = 0; i < nodes.Length; i++)
+        for (int i = 0; i < nodes.Length; i++)
         {
             //UnityEditor.Handles.DrawWireCube(nodes[i].position, Vector3.one* NodeSize);
             if (i < nodes.Length - 1)
@@ -98,13 +100,13 @@ public class Rail : MonoBehaviour
             }
         }
 
-        for(int i = 0; i < nodes.Length - 1; i++)
+        for (int i = 0; i < nodes.Length - 1; i++)
         {
             float temp = 0;
-            for(int j = 0; j < 20; j++)
+            for (int j = 0; j < 20; j++)
             {
                 float f = 0;
-                if(j > 0)
+                if (j > 0)
                 {
                     f = (float)j / 20;
                 }
@@ -133,5 +135,4 @@ public class Rail : MonoBehaviour
     {
         return nodes[i].name;
     }
-
 }
