24b8710a3463622bfd2a78eea7889fbdc32f0cfb
Matt Smith <102088891@student.swin.edu.au>
2021-10-11 11:51:48 +1100

    update golem wait state; golem no longer always walks; close range behaviour updated

--- Sandbox/Assets/Scripts/PlayerController/GolemStates/AI States/GolemFollowState.cs
+++ Sandbox/Assets/Scripts/PlayerController/GolemStates/AI States/GolemFollowState.cs
@@ -74,16 +74,26 @@ public class GolemFollowState:GolemAIState
         float dist = Mathf.Abs((pos - targPos).x);
         dist -= player.closeDistance;
         followSpeed += dist * player.followSpeedFactor;
+       
         if (followSpeed > player.maxFollowSpeed)
         {
             followSpeed = player.maxFollowSpeed;
         }
 
-        //Move Towards Child
-        Vector3 angle = (targPos - pos).normalized;
-        player.SetVelocityX(followSpeed * angle.x);
-    }
+        Vector3 diff = pos - targPos;
 
+        diff.y = 0;
 
+        if(Mathf.Abs(diff.magnitude) > player.closeDistance)
+        {
+            //Move Towards Child
+            Vector3 angle = (targPos - pos).normalized;
+            player.SetVelocityX(followSpeed * angle.x);
+        }
+        else
+        {
+            player.ChangeState(player.AIWaitState);
+        }
+    } 
 }
 
