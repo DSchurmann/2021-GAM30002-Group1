5a9216b71de1092d5f8730cb5f4f1d47ab1744a3
Daniel Schurmann <2096633@student.swin.edu.au>
2021-11-04 01:34:15 +1100

    Animatic UI
    - Animatic UI works with keyboard/controller
    - Mouse hides when not in use
    - Animatic UI hides after x seconds (currently x = 5)

---
+++ Sandbox/Assets/Scripts/UI/AnimaticUIController.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class AnimaticUIController : MonoBehaviour
+{
+    private GameObject cursor;
+    private Button button;
+
+    private float hideTimer = 0f;
+    [SerializeField] private float hideTime;
+    [SerializeField] private bool hideOnStart = true;
+
+    private int selection;
+
+    [SerializeField] private PlayerInputHandler inputHandler;
+
+
+    private void Start()
+    {
+        //get cursor and button
+        cursor = transform.Find("Cursor").gameObject;
+        button = transform.Find("Button_Skip").GetComponent<Button>();
+
+        //hide cursor and button
+        if(hideOnStart)
+        {
+            cursor.SetActive(false);
+            button.gameObject.SetActive(false);
+            //hide mouse
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+
+        }
+    }
+
+    private void Update()
+    {
+        //if controller/keyboard input, show skip and cursor
+        if(CheckButtonPress())
+        {
+            //hide mouse
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            //show button and cursor
+            button.gameObject.SetActive(true);
+            cursor.SetActive(true);
+            //start timer
+            hideTimer = hideTime;
+
+        }
+        //if mouse input, show mouse and button
+        else if(Mouse.current.wasUpdatedThisFrame)
+        {
+            //show mouse
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            //only show button
+            cursor.SetActive(false);
+            button.gameObject.SetActive(true);
+            //start timer
+            hideTimer = hideTime;
+
+        }
+        //run timer
+        if (hideTimer <= 0)
+        {
+            //hide mouse
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            //hide cursor and button
+            button.gameObject.SetActive(false);
+            cursor.SetActive(false);
+        }
+        else
+        {
+            //decrement timer
+            hideTimer -= Time.deltaTime;
+        }
+
+        //Skip if button pressed
+        if (inputHandler.InputMenuAccept)
+        {
+            inputHandler.SetMenuAcceptFalse();
+            button.onClick.Invoke();
+        }
+
+    }
+
+    private bool CheckButtonPress()
+    {
+        bool result = false;
+
+        //check if any keyboard or controller buttons were pressed
+        if(Keyboard.current.wasUpdatedThisFrame)
+        {
+            result = true;
+        }
+        if (Gamepad.current != null)
+        {
+            //get all inputs from all controllers and if a button was pressed
+            foreach (InputControl c in Gamepad.current.allControls)
+            {
+                if (c.IsPressed())
+                {
+                    result = true;
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+}
