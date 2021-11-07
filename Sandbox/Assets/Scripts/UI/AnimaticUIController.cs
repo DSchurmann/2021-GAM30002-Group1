b993d486e620138ebce7e6fd131015458f404d43
Matt Smith <102088891@student.swin.edu.au>
2021-11-07 21:45:01 +1100

    made skip button persistent

--- Sandbox/Assets/Scripts/UI/AnimaticUIController.cs
+++ Sandbox/Assets/Scripts/UI/AnimaticUIController.cs
@@ -10,6 +10,7 @@ public class AnimaticUIController : MonoBehaviour
 
     private float hideTimer = 0f;
     [SerializeField] private float hideTime;
+    [SerializeField] private bool hide = true;
     [SerializeField] private bool hideOnStart = true;
     private bool canSkip = false;
 
@@ -38,56 +39,67 @@ public class AnimaticUIController : MonoBehaviour
 
     private void Update()
     {
-        //if controller/keyboard input, show skip and cursor
-        if(CheckButtonPress())
+        if(hide)
         {
-            //hide mouse
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            //show button and cursor
-            button.gameObject.SetActive(true);
-            cursor.SetActive(true);
-            //start timer
-            hideTimer = hideTime;
+            //if controller/keyboard input, show skip and cursor
+            if (CheckButtonPress())
+            {
+                //hide mouse
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                //show button and cursor
+                button.gameObject.SetActive(true);
+                cursor.SetActive(true);
+                //start timer
+                hideTimer = hideTime;
+                canSkip = true;
+            }
+            //if mouse input, show mouse and button
+            else if (Mouse.current.wasUpdatedThisFrame)
+            {
+                //show mouse
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+                //only show button
+                cursor.SetActive(false);
+                button.gameObject.SetActive(true);
+                //start timer
+                hideTimer = hideTime;
+                canSkip = true;
+            }
+            //run timer
+            if (hideTimer <= 0)
+            {
+                //hide mouse
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+                //hide cursor and button
+                button.gameObject.SetActive(false);
+                cursor.SetActive(false);
+                canSkip = false;
+            }
+            else
+            {
+                //decrement timer
+                hideTimer -= Time.deltaTime;
+            }
 
-        }
-        //if mouse input, show mouse and button
-        else if(Mouse.current.wasUpdatedThisFrame)
-        {
-            //show mouse
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            //only show button
-            cursor.SetActive(false);
-            button.gameObject.SetActive(true);
-            //start timer
-            hideTimer = hideTime;
-            canSkip = true;
-        }
-        //run timer
-        if (hideTimer <= 0)
-        {
-            //hide mouse
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            //hide cursor and button
-            button.gameObject.SetActive(false);
-            cursor.SetActive(false);
-            canSkip = false;
+            //Skip if button pressed
+            if (inputHandler.InputMenuAccept && canSkip)
+            {
+                inputHandler.SetMenuAcceptFalse();
+                button.onClick.Invoke();
+            }
         }
         else
         {
-            //decrement timer
-            hideTimer -= Time.deltaTime;
+            //Skip if button pressed
+            if (inputHandler.InputMenuAccept)
+            {
+                inputHandler.SetMenuAcceptFalse();
+                button.onClick.Invoke();
+            }
         }
-
-        //Skip if button pressed
-        if (inputHandler.InputMenuAccept && canSkip)
-        {
-            inputHandler.SetMenuAcceptFalse();
-            button.onClick.Invoke();
-        }
-
     }
 
     private bool CheckButtonPress()
